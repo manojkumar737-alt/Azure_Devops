06f9de6b1eee19ec2636d185f5c97fca2a29d7eb
krishna <krishna@192.168.1.58>
2026-06-08 17:19:52 +0530

    add v_net practice

--- Azure_Devops/backend_block/main.tf
+++ Azure_Devops/backend_block/main.tf
@@ -5,41 +5,53 @@ terraform{
 			source = "hashicorp/azurerm"
 			version= "4.69.0"
 			}
+      random = {
+      source  = "hashicorp/random"
+      version = "~> 3.7"
+      }
 		}
 }
 #configure the azure providers
 provider "azurerm" {
 	features {}
 }
-terraform {
-  backend "azurerm" {
-    use_cli              = true                                    # Can also be set via `ARM_USE_CLI` environment variable.
-    storage_account_name = "mkstor4wzcbn"                              # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
-    container_name       = "tfstate"                               # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
-    key                  = "prod.terraform.tfstate"                # Can be passed via `-backend-config=`"key=<blob key name>"`
-    resource_group_name  = "mkrg-resources"
-  }
-}
+
 resource "random_string" "sa" {
   length  = 6
   upper   = false
   special = false
 }
 
-resource "azurerm_resource_group" "mk_rg" {
-  name     = "mkrg-resources"
-  location = "West Europe"
+resource "azurerm_resource_group" "backend" {
+  name     = "terraform-backend-rg"
+  location = "East US"
 }
-##### using explicit depandancy ####
 
-resource "azurerm_storage_account" "mk_sa4" {
-  name = "mkstor${random_string.sa.result}"
-  resource_group_name      = "mkrg-resources"
-  location                 = "West Europe"
+resource "azurerm_storage_account" "backend" {
+  name                     = "mkterraformstate1fx9xu"
+  resource_group_name      = azurerm_resource_group.backend.name
+  location                 = azurerm_resource_group.backend.location
   account_tier             = "Standard"
-  account_replication_type = "GRS"
-  depends_on = [
-    azurerm_resource_group.mk_rg
-  ]
+  account_replication_type = "LRS"
 }
+
+resource "azurerm_storage_container" "backend" {
+  name                 = "tfstate"
+  storage_account_id   = azurerm_storage_account.backend.id
+  container_access_type = "private"
+}
+
+### after creating above respouce we can use these resource to configure the backend in terraform block.
+## we can also use the output of these resource to configure the backend. 
+####but here i am using the hard coded value for backend configuration.
+#
+# terraform {
+#   backend "azurerm" {
+#     use_cli              = true
+#     resource_group_name  = "terraform-backend-rg"
+#     storage_account_name = "mkterraformstate1fx9xu"
+#     container_name       = "tfstate"
+#     key                  = "vnet-practice.tfstate"
+#   }
+# }
 
